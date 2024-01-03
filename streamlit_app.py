601a097556be0f4ac5453452361a124a4c3c3dda
Chimdinma <143607814+CAIKAYUBAKA@users.noreply.github.com>
2024-01-03 13:19:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 
 streamlit.title('The happiest day of my life')
-I found Jesus, received Him into my life
-He gave me, Peace, Joy, and hope for tomorrow.
+I found Jesus received Him into my life
+
