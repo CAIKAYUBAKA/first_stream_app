e7a67ca092c87429680bda7278f59928665fa62b
Chimdinma <143607814+CAIKAYUBAKA@users.noreply.github.com>
2024-01-03 13:38:03 -0600

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
+
 import streamlit
 
 streamlit.title('The happiest day of my life,I found Jesus,received Him into my life')
+
+streamlit.header('God judges us according to our works')
+streamlit.text('And He rewards each man accordingly to what he/she has done')
+streamlit.text('The ultimate test of greatness is how you treat every human being')
+streamlit.text('If you have holy shamelessness, you would not care what people think or say')
 
