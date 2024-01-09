15407aeb6b9e51064a41e5b2f2bd5828c4de64d9
Chimdinma <143607814+CAIKAYUBAKA@users.noreply.github.com>
2024-01-09 11:25:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
 #snowflake_related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
-        my_cur_execute("select * from fruit_load_list")
+        my_cur.execute("SELECT * FROM fruit_load_list")
         return my_cur.fetchall()
 
 #Add a button to load the fruit
