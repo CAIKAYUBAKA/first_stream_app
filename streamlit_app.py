20155cd6bf72e7580860f68ddc803e7fca208789
Chimdinma <143607814+CAIKAYUBAKA@users.noreply.github.com>
2024-01-09 14:08:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,17 @@ if streamlit.button('Get Fruit List'):
     my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error ("Please select a fruit to get information")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URLError as e:
+    streamlit.error()
+
 # allow the end user to add a fruit to the list   
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
